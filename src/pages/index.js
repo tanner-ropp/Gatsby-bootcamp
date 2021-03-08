b985af0c489f4bbc32977110e09ec0d3877231e6
Tanner Ropp <tanner.ropp@ufl.edu>
2021-03-07 23:51:54 -0500

    imported Link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 export default function Home() {
-  return <div>Hello world!??</div>
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <h2>I'm Tanner, a JAM stack developer and musician graduating from the University of Florida.</h2>
+      <p>Need a better developer? <Link to="/contact">Contact <em>me.</em></Link></p>
+    </div>
+  )
 }
