bfef930a53f45a632c770a52571427d6f2e48144
Tanner Ropp <tanner.ropp@ufl.edu>
2021-03-10 00:22:45 -0500

    styled header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,21 +5,25 @@ import * as headerStyles from './header.module.scss';
 
 function Header(props) {
     return (
-        <header>
-            <h1>Tanner Ropp</h1>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link to="/" className={headerStyles.title}>
+                    Tanner Ropp
+                </Link>
+            </h1>
             <nav>
-                <ul>
+                <ul className={headerStyles.navList}>
                     <li>
-                        <Link className={headerStyles.link} to="/">Home</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/blog">Blog</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link>
                     </li>
                     <li>
-                        <Link to="/contact">Contact</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link>
                     </li>
                 </ul>
             </nav>
